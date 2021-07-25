91865f670c6f402b92666835ac99a0b604f947ac
tone <tony.y.gong@gmail.com>
2021-07-24 17:37:26 -0700

    bind side buttons to home/end

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
+#![deny(unused_results)]
+
 use argh::FromArgs;
-use evdev_rs::enums::{EventCode, EV_KEY, EV_REL, EV_SYN};
+use evdev_rs::enums::{EventCode, EV_KEY, EV_REL};
 use evdev_rs::{DeviceWrapper as _, InputEvent, UInputDevice};
-use evdev_utils::{DeviceWrapperExt as _};
+use evdev_utils::AsyncDevice;
+use evdev_utils::{DeviceWrapperExt as _, UInputExt as _};
 use futures::{select, StreamExt as _, TryStreamExt as _};
 use log::{debug, info, trace};
-use evdev_utils::AsyncDevice;
 
 #[derive(FromArgs)]
 /// SC2 input remapping arguments.
@@ -37,27 +39,6 @@ struct State {
     held: bool,
 }
 
-fn inject_event(l: &UInputDevice, event_code: EventCode, value: i32) -> std::io::Result<()> {
-    let event = InputEvent {
-        event_code,
-        value,
-        time: evdev_rs::TimeVal {
-            tv_sec: 0,
-            tv_usec: 0,
-        },
-    };
-    info!("injecting event: {:?} {:?}", event_code, value);
-    l.write_event(&event)
-}
-
-fn inject_btn(l: &UInputDevice, btn: EV_KEY) -> std::io::Result<()> {
-    let () = inject_event(&l, EventCode::EV_KEY(btn), 1)?;
-    let () = inject_event(&l, EventCode::EV_SYN(EV_SYN::SYN_REPORT), 0)?;
-    let () = inject_event(&l, EventCode::EV_KEY(btn), 0)?;
-    let () = inject_event(&l, EventCode::EV_SYN(EV_SYN::SYN_REPORT), 0)?;
-    Ok(())
-}
-
 fn log_event(event: &InputEvent) {
     match event.event_code {
         EventCode::EV_MSC(_) | EventCode::EV_SYN(_) | EventCode::EV_REL(_) => {
@@ -67,10 +48,12 @@ fn log_event(event: &InputEvent) {
     }
 }
 
+const REPEAT_KEY: EV_KEY = EV_KEY::KEY_5;
+
 fn main() {
     let Args { log_level } = argh::from_env();
 
-    let () = simple_logger::SimpleLogger::new()
+    simple_logger::SimpleLogger::new()
         .with_level(log::LevelFilter::Warn)
         .with_module_level(std::module_path!(), log_level)
         .init()
@@ -81,8 +64,12 @@ fn main() {
     info!("found mouse {:?} and keyboard {:?}", mouse_path, keeb_path);
 
     let uninit_device = evdev_rs::UninitDevice::new().expect("failed to create uninit device");
-    let () = uninit_device.enable_keys().expect("failed to enable keyboard functionality");
-    let () = uninit_device.enable_mouse().expect("failed to enable mouse functionality");
+    uninit_device
+        .enable_keys()
+        .expect("failed to enable keyboard functionality");
+    uninit_device
+        .enable_mouse()
+        .expect("failed to enable mouse functionality");
     uninit_device.set_name("sc2input");
     uninit_device.set_product_id(1);
     uninit_device.set_vendor_id(1);
@@ -94,7 +81,7 @@ fn main() {
         AsyncDevice::new(keeb_path).expect("failed to create keyboard device"),
         AsyncDevice::new(mouse_path).expect("failed to create mouse device"),
     );
-    let () = keeb_device
+    keeb_device
         .grab(evdev_rs::GrabMode::Grab)
         .expect("failed to grab keyboard device");
 
@@ -103,7 +90,7 @@ fn main() {
         let mut keeb_device = keeb_device.fuse();
         let mut state = State {
             current_key: None,
-            next_key: EV_KEY::KEY_GRAVE,
+            next_key: REPEAT_KEY,
             held: false,
         };
         loop {
@@ -124,13 +111,23 @@ fn main() {
                     match event_code {
                         // scroll up
                         EventCode::EV_REL(EV_REL::REL_WHEEL) if value == 1 => {
-                            inject_btn(&l, EV_KEY::KEY_PAGEUP)
+                            l.inject_key_press(EV_KEY::KEY_PAGEUP)
                                 .expect("failed to inject pageup on scrollup");
                         }
                         // scroll down
                         EventCode::EV_REL(EV_REL::REL_WHEEL) if value == -1 => {
-                            inject_btn(&l, EV_KEY::KEY_PAGEDOWN)
+                            l.inject_key_press(EV_KEY::KEY_PAGEDOWN)
                                 .expect("failed to inject pagedown on scrolldown");
+                        }
+                        // forward side btn
+                        EventCode::EV_KEY(EV_KEY::BTN_EXTRA) => {
+                            l.inject_key_syn(EV_KEY::KEY_HOME, value)
+                                .expect("failed to inject home on forward side button");
+                        }
+                        // back side btn
+                        EventCode::EV_KEY(EV_KEY::BTN_SIDE) => {
+                            l.inject_key_syn(EV_KEY::KEY_END, value)
+                                .expect("failed to inject end on back side button");
                         }
                         _ => {}
                     }
@@ -147,21 +144,21 @@ fn handle_keeb_event(event: InputEvent, l: &UInputDevice, state: &mut State) {
         value,
     } = event;
     match event_code {
-        EventCode::EV_KEY(EV_KEY::KEY_GRAVE) if value == 0 => {
+        EventCode::EV_KEY(REPEAT_KEY) if value == 0 => {
             if let Some(current_key) = state.current_key.take() {
-                inject_event(&l, EventCode::EV_KEY(current_key), value)
+                l.inject_event(EventCode::EV_KEY(current_key), value)
                     .expect("failed to rewrite grave release event");
             } else {
-                let () = l.write_event(&event).expect("failed to forward event");
+                l.write_event(&event).expect("failed to forward event");
             }
         }
-        EventCode::EV_KEY(EV_KEY::KEY_GRAVE) => {
+        EventCode::EV_KEY(REPEAT_KEY) => {
             state.current_key = Some(state.next_key);
             if value == 1 && state.held {
-                inject_event(&l, EventCode::EV_KEY(state.next_key), 0)
+                l.inject_event(EventCode::EV_KEY(state.next_key), 0)
                     .expect("failed to inject artificial release before grave press event");
             }
-            inject_event(&l, EventCode::EV_KEY(state.next_key), value)
+            l.inject_event(EventCode::EV_KEY(state.next_key), value)
                 .expect("failed to rewrite grave press/repeat event");
         }
         EventCode::EV_KEY(k) => {
@@ -174,15 +171,15 @@ fn handle_keeb_event(event: InputEvent, l: &UInputDevice, state: &mut State) {
                     state.next_key = mapped;
                     state.held = true;
                     if state.current_key == Some(mapped) {
-                        inject_event(&l, event_code, 0)
+                        l.inject_event(event_code, 0)
                             .expect("failed to inject artificial release before key press event");
                     }
                 }
             }
-            let () = l.write_event(&event).expect("failed to forward event");
+            l.write_event(&event).expect("failed to forward event");
         }
         _ => {
-            let () = l.write_event(&event).expect("failed to forward event");
+            l.write_event(&event).expect("failed to forward event");
         }
     }
 }
