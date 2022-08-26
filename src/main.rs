5c0d1b3c3778343818241561d0588445d1fa30ed
Tony Gong <gr8tony@hotmail.com>
2022-08-26 00:54:43 -0700

    major changes

--- src/main.rs
+++ src/main.rs
@@ -18,7 +18,7 @@ struct Args {
 
 fn log_event(event: &InputEvent) {
     match event.event_code {
-        EventCode::EV_MSC(_) | EventCode::EV_SYN(_) | EventCode::EV_REL(_) => {
+        EventCode::EV_MSC(_) | EventCode::EV_SYN(_) | EventCode::EV_REL(EV_REL::REL_X) | EventCode::EV_REL(EV_REL::REL_Y) => {
             trace!("event: {:?}", event)
         }
         _ => debug!("event: {:?}", event),
@@ -40,54 +40,65 @@ fn main() {
     }));
     pidlock.acquire().unwrap();
 
-    let mouse_path = futures::executor::block_on(evdev_utils::identify_mouse())
-        .expect("failed to identify mouse");
-    info!("found mouse {:?}", mouse_path);
+    loop {
+        let mouse_path = loop {
+            log::info!("waiting");
+            match futures::executor::block_on(evdev_utils::identify_mouse()) {
+                Ok(mouse_path) => break mouse_path,
+                Err(e) => log::warn!("failed to identify mouse: {}", e),
+            }
+        };
+        info!("found mouse {:?}", mouse_path);
 
-    let uninit_device = evdev_rs::UninitDevice::new().expect("failed to create uninit device");
-    uninit_device
-        .enable_keys()
-        .expect("failed to enable keyboard functionality");
-    uninit_device.set_name("sc2input");
-    uninit_device.set_product_id(1);
-    uninit_device.set_vendor_id(1);
-    uninit_device.set_bustype(3);
-    let l =
-        UInputDevice::create_from_device(&uninit_device).expect("failed to create uinput device");
+        let uninit_device = evdev_rs::UninitDevice::new().expect("failed to create uninit device");
+        uninit_device
+            .enable_keys()
+            .expect("failed to enable keyboard functionality");
+        uninit_device.set_name("sc2input");
+        uninit_device.set_product_id(1);
+        uninit_device.set_vendor_id(1);
+        uninit_device.set_bustype(3);
+        let l =
+            UInputDevice::create_from_device(&uninit_device).expect("failed to create uinput device");
 
-    let mouse_device = AsyncDevice::new(mouse_path).expect("failed to create mouse device");
+        let mouse_device = AsyncDevice::new(mouse_path).expect("failed to create mouse device");
 
-    futures::executor::block_on(mouse_device.try_for_each(|mouse_event| {
-        log_event(&mouse_event);
-        let InputEvent {
-            time: _,
-            event_code,
-            value,
-        } = mouse_event;
-        match event_code {
-            // scroll up
-            EventCode::EV_REL(EV_REL::REL_WHEEL) if value == 1 => {
-                l.inject_key_press(EV_KEY::KEY_UP)
-                    .expect("failed to inject pageup on scrollup");
+        let mut drag_scroll_held = false;
+        let r = futures::executor::block_on(mouse_device.try_for_each(|mouse_event| {
+            log_event(&mouse_event);
+            let InputEvent {
+                time: _,
+                event_code,
+                value,
+            } = mouse_event;
+            match event_code {
+                // middle click
+                EventCode::EV_KEY(EV_KEY::BTN_MIDDLE) => {
+                    drag_scroll_held = value == 1;
+                }
+                // scroll up
+                EventCode::EV_REL(EV_REL::REL_WHEEL) if value == 1 => {
+                    if !drag_scroll_held {
+                        debug!("injecting UP");
+                        l.inject_key_press(EV_KEY::KEY_UP)
+                            .expect("failed to inject up on scrollup");
+                    }
+                }
+                // scroll down
+                EventCode::EV_REL(EV_REL::REL_WHEEL) if value == -1 => {
+                    if !drag_scroll_held {
+                        debug!("injecting DOWN");
+                        l.inject_key_press(EV_KEY::KEY_DOWN)
+                            .expect("failed to inject down on scrolldown");
+                    }
+                }
+                EventCode::EV_KEY(EV_KEY::BTN_SIDE) if value == 1 => {
+                    log::info!("status: {:?}", std::process::Command::new("/home/tone/.local/bin/side_btn.sh").status());
+                }
+                _ => {}
             }
-            // scroll down
-            EventCode::EV_REL(EV_REL::REL_WHEEL) if value == -1 => {
-                l.inject_key_press(EV_KEY::KEY_DOWN)
-                    .expect("failed to inject pagedown on scrolldown");
-            }
-            // forward side btn
-            EventCode::EV_KEY(EV_KEY::BTN_EXTRA) => {
-                l.inject_key_syn(EV_KEY::KEY_END, value)
-                    .expect("failed to inject home on forward side button");
-            }
-            // back side btn
-            EventCode::EV_KEY(EV_KEY::BTN_SIDE) => {
-                l.inject_key_syn(EV_KEY::KEY_PAGEDOWN, value)
-                    .expect("failed to inject end on back side button");
-            }
-            _ => {}
-        }
-        futures::future::ok(())
-    }))
-    .expect("event loop exited");
+            futures::future::ok(())
+        }));
+        log::warn!("mouse event loop ended with: {:?}", r);
+    }
 }
